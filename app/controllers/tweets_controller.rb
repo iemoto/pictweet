cd634f141d4b59add99b9fba14290415dd62884a
iemoto <57856680+iemoto@users.noreply.github.com>
2020-04-12 17:46:38 +0900

    nickname

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :destroy, :show]
+  before_action :move_to_index, except: [:index, :show]
   def index
     @tweets = Tweet.all
   end
@@ -34,5 +35,9 @@ class TweetsController < ApplicationController
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
